7880cce765af6f9fb79be0facc5fe56ed35aa0f0
Martin Jahr <it@raum-fuer-natuerliches-lernen.de>
2025-10-17 19:16:17 +0200

    fix test

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -3,8 +3,7 @@ describe('Freie Schule Ostfriesland website', () => {
     cy.visit('/')
   })
   it('displays the school name', () => {
-    cy.get('h1')
-    .contains('Wir sind die Freie Schule Ostfriesland!');
+    cy.contains('Wir sind die Freie Schule Ostfriesland!');
   })
   it('displays the hero heading', () => {
     cy.contains('Freie Schule Ostfriesland');
